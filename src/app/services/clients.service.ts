4264bb67add6a3cac2f4040629e095c8be249ff9
kretmatt <mrtthias@gmail.com>
2021-01-25 12:15:01 +0100

    Update readme.md file

--- src/app/services/clients.service.ts
+++ src/app/services/clients.service.ts
@@ -3,12 +3,13 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { Client } from '../interfaces/client';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class ClientsService {
   constructor(private http: HttpClient) { }
   getClients(){
-    return this.http.get('https://wifipersontracker.herokuapp.com/test/clients');
+    return this.http.get(environment.apiURL+'/test/clients');
   }
 }
